17197add294e010235f6118c252c2a4034e27984
Dean <72749793+drossano@users.noreply.github.com>
2022-11-03 12:17:40 -0400

    Generalize board tests

--- spec/board_test.rb
+++ spec/board_test.rb
@@ -4,7 +4,8 @@ describe Board do
   describe "#initialize" do
     subject(:board) { described_class.new }
     it "creates an 8 x 8 array" do
-      expect(board.board_array).to eq([[nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil], [nil, nil, nil, nil, nil, nil, nil, nil]])
+      expect(board.board_array.length).to eq(8)
+      expect(board.board_array[0..8].length).to eq(8)
     end
   end
 end
