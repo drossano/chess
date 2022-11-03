42f736075e38dc531ff35873a7f7e7982b029e38
Dean <72749793+drossano@users.noreply.github.com>
2022-11-03 12:59:37 -0400

    Add Space class

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,8 @@
+require_relative "../lib/space"
 class Board
   attr_reader :board_array
 
   def initialize
-    @board_array = Array.new(8,Array.new(8))
+    @board_array = Array.new(8, Array.new(8, Space.new))
   end
 end
--- spec/board_test.rb
+++ spec/board_test.rb
@@ -3,9 +3,17 @@ require_relative "../lib/board"
 describe Board do
   describe "#initialize" do
     subject(:board) { described_class.new }
-    it "creates an 8 x 8 array" do
+    it "creates an 8 elements long array" do
       expect(board.board_array.length).to eq(8)
-      expect(board.board_array[0..8].length).to eq(8)
+      expect(board.board_array).to be_an(Array)
+    end
+    it "creates sub-arrays that are 8 elements long" do
+      sub_array = board.board_array[0]
+      expect(sub_array.length).to eq(8)
+      expect(sub_array).to be_an(Array)
+    end
+    it "creates Space objects in each of those elements" do
+      expect(board.board_array[0][0]).to be_a(Space)
     end
   end
 end
