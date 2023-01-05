e101f507396b962ef6ebb5e0492adb9019e861c6
Dean <72749793+drossano@users.noreply.github.com>
2023-01-05 12:52:27 -0500

    Add all knight moves

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -8,4 +8,39 @@ class Knight < Piece
     @rank = move_vertical(@rank, 2)
     @file = move_horizontal(@file, 1)
   end
+
+  def move_up_two_left_one
+    @rank = move_vertical(@rank, 2)
+    @file = move_horizontal(@file, -1)
+  end
+
+  def move_right_two_up_one
+    @rank = move_vertical(@rank, 1)
+    @file = move_horizontal(@file, 2)
+  end
+
+  def move_right_two_down_one
+    @rank = move_vertical(@rank, -1)
+    @file = move_horizontal(@file, 2)
+  end
+
+  def move_down_two_right_one
+    @rank = move_vertical(@rank, -2)
+    @file = move_horizontal(@file, 1)
+  end
+
+  def move_down_two_left_one
+    @rank = move_vertical(@rank, -2)
+    @file = move_horizontal(@file, -1)
+  end
+
+  def move_left_two_down_one
+    @rank = move_vertical(@rank, -1)
+    @file = move_horizontal(@file, -2)
+  end
+
+  def move_left_two_up_one
+    @rank = move_vertical(@rank, 1)
+    @file = move_horizontal(@file, -2)
+  end
 end
--- spec/knight_test.rb
+++ spec/knight_test.rb
@@ -1,15 +1,87 @@
 require_relative "../lib/pieces/knight"
 
 describe Knight do
-  describe "#move_up_two_right_one" do
+  context "for a black knight starting at rank 0 file 0" do
     subject(:knight) {described_class.new("black", 0, 0)}
-  it "increases the rank by two" do
-    knight.move_up_two_right_one
-    expect(knight.rank).to eq(2)
-  end
-  it "increases the file by one" do
-    knight.move_up_two_right_one
-    expect(knight.file).to eq(1)
-  end
+    describe "#move_up_two_right_one" do
+      it "increases the rank by two" do
+        knight.move_up_two_right_one
+        expect(knight.rank).to eq(2)
+      end
+      it "increases the file by one" do
+        knight.move_up_two_right_one
+        expect(knight.file).to eq(1)
+      end
+    end
+    describe "#move_up_two_left_one" do
+      it "increases the rank by two" do
+        knight.move_up_two_left_one
+        expect(knight.rank).to eq(2)
+      end
+      it "decreases the file by one" do
+        knight.move_up_two_left_one
+        expect(knight.file).to eq(-1)
+      end
+    end
+    describe "#move_right_two_up_one" do
+      it "increases the rank by one" do
+        knight.move_right_two_up_one
+        expect(knight.rank).to eq(1)
+      end
+      it "increases the file by two" do
+        knight.move_right_two_up_one
+        expect(knight.file).to eq(2)
+      end
+    end
+    describe "#move_right_two_down_one" do
+      it "decreases the rank by one" do
+        knight.move_right_two_down_one
+        expect(knight.rank).to eq(-1)
+      end
+      it "increases the file by two" do
+        knight.move_right_two_down_one
+        expect(knight.file).to eq(2)
+      end
+    end
+    describe "#move_down_two_right_one" do
+      it "decreases the rank by two" do
+        knight.move_down_two_right_one
+        expect(knight.rank).to eq(-2)
+      end
+      it "increases the file by one" do
+        knight.move_down_two_right_one
+        expect(knight.file).to eq(1)
+      end
+    end
+    describe "#move_down_two_left_one" do
+      it "decreases the rank by two" do
+        knight.move_down_two_left_one
+        expect(knight.rank).to eq(-2)
+      end
+      it "decreases the file by one" do
+        knight.move_down_two_left_one
+        expect(knight.file).to eq(-1)
+      end
+    end
+    describe "#move_left_two_down_one" do
+      it "decreases the rank by one" do
+        knight.move_left_two_down_one
+        expect(knight.rank).to eq(-1)
+      end
+      it "decreases the file by two" do
+        knight.move_left_two_down_one
+        expect(knight.file).to eq(-2)
+      end
+    end
+    describe "#move_left_two_up_one" do
+      it "increases the rank by one" do
+        knight.move_left_two_up_one
+        expect(knight.rank).to eq(1)
+      end
+      it "decreases the file by two" do
+        knight.move_left_two_up_one
+        expect(knight.file).to eq(-2)
+      end
+    end
   end
 end
